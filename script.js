ba20e74aea31bc71f64f4f54fb282f6ed2db89e8
Niewolinsky <main.niewolinski@gmail.com>
2020-01-12 21:22:13 +0100

    12012020

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ import MagicGrid    from '/web_modules/magic-grid.js';
 import { render }   from '/web_modules/timeago.js';
 import { throttle } from '/web_modules/tiny-throttle.js'
 import '/web_modules/particles.js';
-
 particlesJS.load('particles-js', 'particlesconfig.json')
- 
+
+const LOCAL_STORAGE_TITLE_KEY = 'todo.titles';
+const LOCAL_STORAGE_NOTE_KEY = 'todo.notes';
+const LOCAL_STORAGE_PRIORITY_KEY = 'todo.priorities';
+const LOCAL_STORAGE_DATE_KEY = 'todo.dates';
+
+let titles = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TITLE_KEY)) || [];
+let notes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_NOTE_KEY)) || [];
+let priorities = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PRIORITY_KEY)) || [];
+let dates = JSON.parse(localStorage.getItem(LOCAL_STORAGE_DATE_KEY)) || [];
+
 let timestamps = document.querySelectorAll('.timeago')
 
 let magicGrid = new MagicGrid({
@@ -16,7 +25,18 @@ let magicGrid = new MagicGrid({
     static: true,
     useMin: true
 })
-magicGrid.listen()
+magicGrid.listen();
+
+for (let i = 0; i < titles.length; i++) {
+  addNote(titles[i], notes[i], priorities[i], dates[i]);
+}
+
+function kaszka() {
+    for (let i = 0; i < 2; i++) {
+      addNote(titles[i], notes[i], priorities[i], dates[i]);
+    }
+    console.log(titles);
+}
 
 function isEmptyOrSpaces(str){
   return str === null || str.match(/^\s*$/) !== null;
@@ -26,6 +46,7 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        kaszka();
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
@@ -51,7 +72,7 @@ let x = function() {
 window.addEventListener('scroll', throttle(x, 1000))
 // !FIX THIS
 
-function addNote(title, note, checked) {
+function addNote(title, note, priority, cacheLoad = true, date = Date.now()) {
   if (isEmptyOrSpaces(title) && isEmptyOrSpaces(note)) {
     let notification = document.createElement("div");
     notification.classList.add('notification');
@@ -75,6 +96,13 @@ function addNote(title, note, checked) {
     return;
   }
 
+  if (cacheLoad == false) {
+    titles.push(title);
+    notes.push(note);
+    priorities.push(priority);
+    dates.push(date);
+  }
+
   let card = document.createElement("div");
   card.className = "card";
 
@@ -82,7 +110,7 @@ function addNote(title, note, checked) {
   removeLogo.src="svgs/remove.svg"
   removeLogo.alt="Remove Logo"
   removeLogo.className = "removeLogo";
-  removeLogo.classList.add(checked);
+  removeLogo.classList.add(priority);
 
   let contentDiv = document.createElement("div");
   contentDiv.className = "cardContent";
@@ -100,7 +128,7 @@ function addNote(title, note, checked) {
   timeLogo.className = "clockLogo";
   let timeDiv = document.createElement("div");
   timeDiv.classList.add("timeago");
-  timeDiv.setAttribute("datetime", Date.now());
+  timeDiv.setAttribute("datetime", date);
 
   timewrapDiv.appendChild(timeLogo);
   timewrapDiv.appendChild(timeDiv);
@@ -113,6 +141,11 @@ function addNote(title, note, checked) {
   card.appendChild(contentDiv);
 
   cardContainer.appendChild(card);
+
+  localStorage.setItem(LOCAL_STORAGE_TITLE_KEY, JSON.stringify(titles));
+  localStorage.setItem(LOCAL_STORAGE_NOTE_KEY, JSON.stringify(notes));
+  localStorage.setItem(LOCAL_STORAGE_PRIORITY_KEY, JSON.stringify(priorities));
+  localStorage.setItem(LOCAL_STORAGE_DATE_KEY, JSON.stringify(dates));
 
   magicGrid = new MagicGrid({
     container: '#cardContainer',
@@ -135,6 +168,7 @@ addButton.onclick = function(event) {
 }
 
 function delNote(note) {
+  //!DODAJ USUWANIE NOTATKI Z CACHE, ZROB MOZE DATA-ATTRIBUTE DO KAZDEJ NOTATKI
   note.parentNode.parentNode.removeChild(note.parentNode);
 
   magicGrid = new MagicGrid({
@@ -162,8 +196,8 @@ newNoteModal.onclick = function(event) {
   }
   if (event.target.classList.contains("modalAcceptLogo")) {
     newNoteModal.style.display = "none";
-    let checked = document.querySelector('input[name="priority"]:checked').value;
-    addNote(title1.value, note1.value, checked);
+    let priority = document.querySelector('input[name="priority"]:checked').value;
+    addNote(title1.value, note1.value, priority, false);
     modalCardContent.reset();
     modalBackground.classList.remove("boxblur");
     document.getElementsByClassName('particles-js-canvas-el')[0].classList.remove("boxblur");
